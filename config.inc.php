0733fc2a7810aaad73883a77bfcb8363341e7e67
Jeevakirtan <Jeevakirtan@gmail.com>
2018-07-27 00:26:57 +0530

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -28,7 +28,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = '10.129.35.28';
+$cfg['Servers'][$i]['host'] = '35.183.56.68';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
